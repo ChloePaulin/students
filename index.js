06169cc4c9809cb678768aca9e79ebbc7ed0c2c1
chloe974 <chloe.previl@gmail.com>
2025-08-04 11:38:32 +0200

    Tentative de boucles

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const students = ["Ethan", "Maryam", "Fatma", "Aline"];
+
+// const colors = ["blue", "red", "green", "yellow"];
 import chalk from "chalk";
+
+// function loop(table){
+//     table.forEach(line => {
+//             console.log(chalk.blue(line));
+//             console.log(chalk.red(line));
+//             console.log(chalk.green(line));
+//             console.log(chalk.yellow(line));
+//     });
+// }
+
+// loop(students);
 
 console.log(chalk.blue(students[0]));
 console.log(chalk.red(students[1]));
