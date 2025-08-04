d91ef51c5c3399f9a9707cd2df689d939a64f65c
chloe974 <chloe.previl@gmail.com>
2025-08-04 11:26:51 +0200

    Afficher 4 étudiants de couleur différentes

---
+++ index.js
@@ -0,0 +1,7 @@
+const students = ["Ethan", "Maryam", "Fatma", "Aline"];
+import chalk from "chalk";
+
+console.log(chalk.blue(students[0]));
+console.log(chalk.red(students[1]));
+console.log(chalk.green(students[2]));
+console.log(chalk.yellow(students[3]));
